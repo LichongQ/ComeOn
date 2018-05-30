3c3cc2d65bd632596b007b7a8dedfe28f342d479
lichongq <lichong199@163.com>
2018-05-30 08:08:27 +0800

    update

--- Main.java
+++ Main.java
@@ -8,29 +8,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Boolean result = false;
+
         while (in.hasNextLine()) {//注意while处理多个case
 
             String str = in.nextLine();
             String[] strArray = str.split(" ");
             strArray[0] = strArray[0].substring(1);
-            if (strArray[0].length() == 0) {
-                System.out.println(result);
-                continue;
-            }
+
             strArray[strArray.length - 2] = strArray[strArray.length - 2].
                     substring(0, strArray[strArray.length - 2].length() - 1);
-            if (strArray[strArray.length - 2].length() == 0) {
-                System.out.println(result);
-                continue;
-            }
 
-            for (int jj=0;jj<strArray.length-1;jj++)
-            {
-                for (int kk=jj+1; kk<strArray.length-1;kk++)
-                {
-                    if (strArray[jj].length() < strArray[kk].length())
-                    {
+            for (int jj = 0; jj < strArray.length - 1; jj++) {
+                for (int kk = jj + 1; kk < strArray.length - 1; kk++) {
+                    if (strArray[jj].length() < strArray[kk].length()) {
                         String temp = strArray[kk];
                         strArray[kk] = strArray[jj];
                         strArray[jj] = temp;
@@ -38,24 +28,20 @@ public class Main {
                 }
             }
 
-            String targetStr = strArray[strArray.length-1];
-            while(targetStr.length()!= 0)
-            {
+            String targetStr = strArray[strArray.length - 1];
+            while (targetStr.length() != 0) {
                 Boolean match = false;
-                for(int ii=0;ii<strArray.length-1;ii++)
-                {
-                    if(targetStr.startsWith(strArray[ii]))
-                    {
-                        targetStr = targetStr.substring(strArray[ii].length(),targetStr.length());
+                for (int ii = 0; ii < strArray.length - 1; ii++) {
+                    if (strArray[ii].length() != 0 && targetStr.startsWith(strArray[ii])) {
+                        targetStr = targetStr.substring(strArray[ii].length(), targetStr.length());
                         match = true;
                     }
                 }
-                if (!match)
-                {
+                if (!match) {
                     break;
                 }
             }
-            result = targetStr.length()== 0 ? true : false;
+            Boolean result = targetStr.length() == 0;
             System.out.println(result);
         }
 
